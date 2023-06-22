1905b35dc46b943f81dbc7be91ae09ead0afb3ca
487 <2195436316@qq.com>
2023-06-22 19:38:15 +0800

    个人页面优化，搜索功能

---
+++ web/src/views/Search/index.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="headText">
+    <p>以下是 {{ key }} 的搜索结果 :</p>
+  </div>
+  <div class="total">
+    <div class="layer_good">
+      <div class="recommend" v-for="(item, index) in goodsList" :key="index">
+        <router-link
+          :to="{ name: 'Details', params: { goodId: item.productId } }"
+        >
+          <ul>
+            <li>
+              <img :src="item.productLink" class="lazy" />
+            </li>
+            <li class="introduce">{{ item.productName }}</li>
+            <li class="price">￥{{ item.productPrice }}</li>
+          </ul>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import axios from "axios";
+import { onMounted, ref } from "vue";
+import { onBeforeRouteUpdate, useRoute } from "vue-router";
+const goodsList = ref([]);
+const route = useRoute();
+const key = ref(route.params.prom);
+
+const getgoodList = () => {
+  axios({
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    method: "get",
+    url: "/api/Search",
+    data: {
+      name: key,
+      pagenum: 1,
+    },
+  })
+    .then((response) => {
+      goodsList.value = response.data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+onMounted(() => {
+  getgoodList();
+});
+</script>
+
+<style scoped>
+.headText {
+  position: relative;
+  top: 30px;
+  left: 20px;
+  font-size: 20px;
+}
+
+.total {
+  position: relative;
+  top: 50px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+}
+
+.layer_good {
+  text-align: center;
+  flex-grow: 1;
+  margin-bottom: 64px;
+}
+
+.recommend {
+  display: inline-block;
+  width: 300px; /* 设置每个推荐项的宽度 */
+  margin: 12px; /* 设置推荐项之间的间距 */
+  text-align: center;
+}
+
+li {
+  list-style: none;
+}
+
+.recommend img {
+  width: 100%; /* 让图片宽度充满容器 */
+  height: auto; /* 根据宽度自适应调整高度 */
+  text-align: center;
+  box-shadow: 5px 5px 5px rgba(57, 56, 56, 0.5);
+}
+
+.introduce {
+  width: 100%;
+  text-align: center;
+  font-size: 14px;
+  margin-top: 6px;
+  margin-bottom: 6px;
+}
+
+.price {
+  width: 100%;
+  text-align: center;
+  font-size: 18px;
+  color: #f00;
+}
+</style>
