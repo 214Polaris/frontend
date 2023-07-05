01f67d376771f54503fd3bb79429308ecc27c488
487 <2195436316@qq.com>
2023-07-05 22:14:14 +0800

    轮播图

--- web/src/views/home/index.vue
+++ web/src/views/home/index.vue
@@ -1,11 +1,13 @@
 <script setup>
 import CarouselMap from "./components/CarouselMap.vue";
-import Recommend from "./components/Recommend.vue"
+import Recommend from "./components/Recommend.vue";
 </script>
 
 <template>
-  <CarouselMap style="margin-top: 20px;"/>
-    <h1 style="color: #collapse-A;margin-top:30px;margin-left: 30px;">为您推荐：</h1>
-    <el-divider />  
+  <CarouselMap style="margin-top: 20px" />
+  <h1 style="color: #collapse-A; margin-top: 30px; margin-left: 30px">
+    为您推荐：
+  </h1>
+  <el-divider />
   <Recommend />
 </template>
